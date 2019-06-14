47d0cf37894c5c619b44291b1b85c934e267244a
Camilov1927 <camilov1927@gmail.com>
2019-06-13 22:14:34 -0500

    First Commit the app destini challenge flutter

---
+++ lib/story.dart
@@ -0,0 +1,9 @@
+class Story {
+  String storyTitle;
+  String choice1Text;
+  String choice2Text;
+  int choice1;
+  int choice2;
+
+  Story({this.storyTitle,this.choice1Text,this.choice2Text,this.choice1,this.choice2});
+}
